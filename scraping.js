24256431fec4dbb8890b4d3925077a7f0ce0d87f
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2023-07-29 12:54:10 -0300

    fix movie poster

--- scraping.js
+++ scraping.js
@@ -21,32 +21,57 @@ exports.scrapingLetterboxd = async (user, month) => {
 
   const arrayMovies = [];
 
-  const htmlElement = $(".diary-entry-row").each((i, element) => {
+  for (element of $(".diary-entry-row")) {
     const movieName = $(element).find(".headline-3").text();
+    let pageLink = $(element).find(".headline-3").find("a").attr("href");
     const movieRating = $(element).find(".rating").text();
-    const movieYear = $(element).find(".td-released").text();
     const isLiked = $(element).find(".icon-liked").length;
 
-    objMovies = {
+    let result = pageLink.split("/");
+    pageLink = result[3];
+
+    objMovie = {
       movieName,
       movieRating,
-      movieYear,
       isLiked,
+      pageLink,
     };
 
-    arrayMovies.push(objMovies);
-  });
+    arrayMovies.push(objMovie);
+  }
 
   for (movie of arrayMovies) {
-    const { movieName, movieYear } = movie;
+    const { movieName, movieYear, pageLink } = movie;
 
+    //fetch director
+    do {
+      var axiosResponse2 = await axios.request({
+        method: "GET",
+        url: `https://letterboxd.com/film/${pageLink}/`,
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+        },
+      });
+    } while (axiosResponse2.length === 0);
+
+    const $ = cheerio.load(axiosResponse2.data);
+
+    let movieId =
+      $(".micro-button:eq(1)").attr("href") ||
+      $(".micro-button:eq(0)").attr("href");
+    let result = movieId.split("/");
+    movieId = result[4];
+
+    //fetch cover
     const res = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieName}&year=${movieYear}`
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
     );
     const json = await res.json();
-    moviePoster = json.results[0].poster_path;
+    const moviePoster = json.poster_path;
 
     movie.moviePoster = moviePoster;
+    delete movie.pageLink;
   }
 
   return arrayMovies;
